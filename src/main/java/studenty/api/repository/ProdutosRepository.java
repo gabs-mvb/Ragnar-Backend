2d1bc7227a9f8f5eb71377b716ebe78dfd509e0f
gabsmvb <gabriel.boas@sptech.school>
2024-01-29 23:55:23 -0300

    reestruturação do projeto

--- src/main/java/studenty/api/repository/ProdutosRepository.java
+++
@@ -1,8 +0,0 @@
-package studenty.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import studenty.api.entity.Produto;
-import studenty.api.viewer.produtos.NomeProduto;
-
-public interface ProdutosRepository extends JpaRepository<Produto,Long> {
-}
